6cb1c9bcee85110aa6a4bac36f3638403bec5a1c
Ruslan Dubrovin <r.dubrovin@infotech.team>
2017-03-21 16:18:21 +0300

    added getenv

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	"bitbucket.infotech.team/gol/goconsul"
 	"fmt"
 	"time"
+	"os"
 )
 
 func main() {
-	consul, err := GoConsul.NewConsulClient("consul.service.consul:8500")
+	consul, err := GoConsul.NewConsulClient(os.Getenv("CONSUL_ADDR"))
 	if err != nil {
 		fmt.Println(err)
 	}
 	go server.Run()
 	time.Sleep(time.Second)
-	_, _, err = consul.Service("Hello", "")
+	_, _, err = consul.Service(os.Getenv("SERVICE_NAME"), os.Getenv("SERVICE_TAG"))
 
 	if err != nil {
 		fmt.Println(err)
